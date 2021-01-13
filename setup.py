73cd0b7c68f3066864b1fb11f81baa68dfd76b2c
Antonio J. Delgado <antoniodelgado@susurrando.com>
2021-01-13 17:58:11 +0200

    Add more metadata

--- setup.py
+++ setup.py
@@ -1,2 +1,12 @@
 import setuptools
-setuptools.setup(scripts=['feed2imap/feed2imap.py'])
+setuptools.setup(
+    scripts=['feed2imap/feed2imap.py'],
+    author="Antonio J. Delgado",
+    version='0.0.1',
+    name='feed2imap',
+    author_email="antonio@susurrando.com",
+    url="https://susurrando.com",
+    description="Get RSS feeds into an IMAP account",
+    license="GPLv2",
+    keywords=["IMAP", "IMAPS", "RSS", "feeds", "RDF", "mail"]
+    )
